035942fc317f5397b452e463810625b56a78bfcf
minhaz000 <mrmminhaz@gamail.com>
2022-08-23 17:59:54 +0600

    adding event handeler

--- js/player.js
+++ js/player.js
@@ -10,6 +10,8 @@ function display (Players,name){
      }
      document.getElementById('display_player').innerHTML = name
 } 
+
+
 // ======== Function for get Player =============================
 function getPlayer (element){
     let playerName = element.parentNode.children[0].innerText
@@ -23,32 +25,40 @@ if(selectedPlayers.length<5){
 else{alert("You can't select more than 5 player !!!")}
 }
 
+
+// ======== Function Player Expense =============================
 function getPlayerExpense(){
- let  costPerPlayer = parseInt(document.getElementById('getPlayerExpense').value)
- 
-if(costPerPlayer>0) {
-    let PlayerExpenses  = selectedPlayers.length*costPerPlayer
-
-    document.getElementById('displayPlayerExpense').innerHTML = PlayerExpenses
-    console.log( typeof costPerPlayer)
-    return PlayerExpenses
-}
-else{
-    alert("You need to input POSITIVE and NUMBER Type value !!! ")
+    let  costPerPlayer = parseInt(document.getElementById('getPlayerExpense').value)
     
-}
-}
-function getToatal () {
-  const players = getPlayerExpense()
-  const manager = parseInt(document.getElementById('cost_manager').value)
-  const coach   = parseInt(document.getElementById('cost_coach').value)
- if(manager>0 && coach>0 ) {
-    const total = players + manager + coach
-    document.getElementById('display_total').innerHTML = total
- }
- else{
-    alert("You need to input POSITIVE and NUMBER Type value !!!")
+   if(costPerPlayer>0) {
+       let PlayerExpenses  = selectedPlayers.length*costPerPlayer
    
-}
+       document.getElementById('displayPlayerExpense').innerHTML = PlayerExpenses
+       console.log( typeof costPerPlayer)
+       return PlayerExpenses
+   }
+   else{
+       alert("You need to input POSITIVE and NUMBER Type value !!! ")
+       
+   }}
 
-}
+
+
+// Get The Player Expense By eventHandeler 
+document.getElementById('Calculate').addEventListener('click', getPlayerExpense) 
+
+
+
+// Get The total of team Cost
+document.getElementById('Total').addEventListener('click', function () {
+    const players = getPlayerExpense()
+    const manager = parseInt(document.getElementById('cost_manager').value)
+    const coach   = parseInt(document.getElementById('cost_coach').value)
+   if(manager>0 && coach>0 ) {
+      const total = players + manager + coach
+      document.getElementById('display_total').innerHTML = total
+   }
+   else{
+      alert("You need to input POSITIVE and NUMBER Type value !!!")
+     
+}})
